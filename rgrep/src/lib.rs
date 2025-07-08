60c15e90f2734e89f1d9b249430b7e4a7ce7129c
kobe4cn <kevin.yang.xgz@gmail.com>
2025-07-08 12:49:18 +0800

    features: update rgrep for async and parallel process. using futures unorder task. tokio spawn, bufreader

--- rgrep/src/lib.rs
+++ rgrep/src/lib.rs
@@ -1,2 +1,84 @@
 mod error;
+
+use std::ops::Range;
+use std::path::Path;
+
+use colored::Colorize;
 pub use error::GrepError;
+
+use clap::Parser;
+use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, Stdout};
+
+#[derive(Debug, Parser)]
+pub struct GrepConfig {
+    pub pattern: String,
+    pub path: Vec<String>,
+}
+
+pub type StrategyFn<W, R> = fn(&Path, BufReader<R>, &Regex, &mut W) -> Result<(), GrepError>;
+
+impl GrepConfig {
+    pub fn match_with_default_strategy(&self) -> Result<(), GrepError> {
+        self.match_with_strategy(default_strategy)
+    }
+    pub fn match_with_strategy(&self, strategy: StrategyFn<Stdout, File>) -> Result<(), GrepError> {
+        let re = Regex::new(&self.pattern)?;
+        let files = &self.path;
+        files.into_iter().for_each(|filename| {
+            if let Ok(file) = File::open(filename) {
+                let reader = BufReader::new(file);
+                let mut stdout = std::io::stdout();
+                if let Err(e) = strategy(Path::new(filename), reader, &re, &mut stdout) {
+                    println!("Internal error: {}", e);
+                }
+            }
+        });
+        Ok(())
+    }
+}
+
+pub fn default_strategy<W: std::io::Write, R: Read>(
+    path: &Path,
+    reader: BufReader<R>,
+    re: &Regex,
+    stdout: &mut W,
+) -> Result<(), GrepError> {
+    let matches: Vec<String> = reader
+        .lines()
+        .enumerate()
+        .map(|(line_number, line)| {
+            line.ok()
+                .map(|line| {
+                    re.find(&line)
+                        .map(|mat| format_line(&line, line_number + 1, mat.range()))
+                })
+                .flatten()
+        })
+        .filter_map(|v| v.ok_or(()).ok())
+        .collect();
+
+    let result = matches.join("\n");
+    if !result.is_empty() {
+        stdout.write(path.display().to_string().as_bytes())?;
+        stdout.write(b"\n")?;
+        stdout.write(result.as_bytes())?;
+        stdout.write(b"\n")?;
+    }
+
+    Ok(())
+}
+
+pub fn format_line(line: &str, lineno: usize, range: Range<usize>) -> String {
+    let Range { start, end } = range;
+    let prefix = &line[..start];
+    format!(
+        "{0: >6}:{1: <3} {2}{3}{4}",
+        lineno.to_string().blue(),
+        (prefix.chars().count() + 1).to_string().cyan(),
+        prefix,
+        &line[start..end].red(),
+        &line[end..]
+    )
+}
--- rgrep/src/main.rs
+++ rgrep/src/main.rs
@@ -3,10 +3,14 @@ use clap::Parser;
 use colored::Colorize;
 use dashmap::DashMap;
 
+use futures::{StreamExt, stream::FuturesUnordered};
 use regex::Regex;
-use rgrep::GrepError;
-use std::ops::Deref;
-use tokio::{fs::File, io::AsyncReadExt};
+use rgrep::{GrepConfig, GrepError};
+use std::{io::Write, ops::Deref};
+use tokio::{
+    fs::File,
+    io::{AsyncBufReadExt, BufReader},
+};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -27,60 +31,112 @@ impl Deref for Rgrep {
         &self.inner
     }
 }
-
+#[allow(unused)]
 impl Rgrep {
     pub async fn match_with_default_strategy(&self) -> Result<(), GrepError> {
         // println!("pattern: {}, path: {:?}", rgrep.pattern, rgrep.path);
         let re = Regex::new(&self.pattern)?;
+        //实现多个文件同时异步搜索
+        let mut tasks = FuturesUnordered::new();
         let dashmap = DashMap::new();
+
         for fs in &self.path {
-            let mut file = File::open(fs).await?;
-            let mut buf = Vec::new();
-            file.read_to_end(&mut buf).await?;
-            let text = String::from_utf8(buf)?;
-            let file_lines = format_line(text, &re);
-            dashmap.insert(fs.clone(), file_lines);
+            let fs_clone = fs.clone();
+            let re_clone = re.clone();
+            
+            tasks.push(tokio::spawn(async move {
+                search_in_file(fs_clone, re_clone).await
+            }));
         }
-        self.print_result(&dashmap);
+
+        while let Some(Ok(result)) = tasks.next().await {
+            let (path, lines) = result?;
+            dashmap.insert(path, lines);
+        }
+        let stdout = std::io::stdout();
+        self.print_result(&dashmap, stdout);
         Ok(())
     }
 
-    pub fn print_result(&self, dashmap: &DashMap<String, Vec<String>>) {
+    pub fn print_result<W: Write>(
+        &self,
+        dashmap: &DashMap<String, Vec<String>>,
+        mut stdout: W,
+    ) -> Result<(), GrepError> {
         for entry in dashmap.iter() {
-            println!("{}:", entry.key());
-            let value = entry.value();
-            for line in value.iter() {
-                println!("    {}", line);
+            writeln!(stdout, "{}:", entry.key())?;
+            for line in entry.value().iter() {
+                writeln!(stdout, " {}", line)?;
             }
         }
+        Ok(())
     }
+
+    // pub fn print_result(&self, dashmap: &DashMap<String, Vec<String>>) {
+    //     for entry in dashmap.iter() {
+    //         println!("{}:", entry.key());
+    //         let value = entry.value();
+    //         for line in value.iter() {
+    //             println!("    {}", line);
+    //         }
+    //     }
+    // }
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let rgrep = Rgrep::parse();
-    rgrep.match_with_default_strategy().await?;
-    Ok(())
-}
-
-pub fn format_line(text: String, re: &Regex) -> Vec<String> {
-    let mut file_lines = Vec::new();
-    for (i, line) in text.lines().enumerate() {
+async fn search_in_file(path: String, re: Regex) -> Result<(String, Vec<String>), GrepError> {
+    let file = File::open(&path).await?;
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+    let mut result = Vec::new();
+    let mut lineno = 0;
+    while let Some(line) = lines.next_line().await? {
+        lineno += 1;
         let mut line_format = String::new();
-        if re.is_match(line) {
-            let highlighted = re.replace_all(line, |caps: &regex::Captures| {
+        if re.is_match(&line) {
+            let highlighted = re.replace_all(&line, |caps: &regex::Captures| {
                 if let Some(cap) = caps.get(0) {
-                    let range = format!("{}:{}", i + 1, cap.start());
-                    line_format = range.clone();
+                    line_format = format!("{}:{}", lineno, cap.start());
                 }
                 format!("{}", &caps[0].red())
             });
-            file_lines.push(format!("{} {}", line_format, highlighted));
+            result.push(format!("{}{}", line_format, highlighted));
         }
     }
-    file_lines
+    Ok((path, result))
 }
 
+// pub fn format_line(text: String, re: &Regex) -> Vec<String> {
+//     let mut file_lines = Vec::new();
+//     for (i, line) in text.lines().enumerate() {
+//         let mut line_format = String::new();
+//         if re.is_match(line) {
+//             let highlighted = re.replace_all(line, |caps: &regex::Captures| {
+//                 if let Some(cap) = caps.get(0) {
+//                     let range = format!("{}:{}", i + 1, cap.start());
+//                     line_format = range.clone();
+//                 }
+//                 format!("{}", &caps[0].red())
+//             });
+//             file_lines.push(format!("{} {}", line_format, highlighted));
+//         }
+//     }
+//     file_lines
+// }
+
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     let rgrep = Rgrep::parse();
+//     rgrep.match_with_default_strategy().await?;
+//     Ok(())
+// }
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let rgrep = GrepConfig::parse();
+    rgrep.match_with_default_strategy()?;
+    Ok(())
+}
+//最简单的写法，但是效率很低，因为每次都要读取文件，然后匹配，然后输出
 // #[tokio::main]
 // async fn main() -> Result<()> {
 //     let rgrep = Rgrep::parse();
